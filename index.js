7810b1022f674229fedd4e872d6c856b6251afe8
AlinaKa7 <7k.alina32@gmail.com>
2023-05-11 16:11:11 +0300

    changed sorting

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ setOfNumbers = parseInt(prompt('Please, enter how many numbers do you want to us
 
 inputArray = [];
 
-for(i = 0; i < setOfNumbers; i++) {
+for (i = 0; i < setOfNumbers; i++) {
     inputArray[i] = parseInt(prompt('Please, enter your number ' + (i+1)));
 }
 
+sortingOrder = (inputArray) => {
+    for (i = 1; i < inputArray.length; i++) {
+        for (j = 0; j < i; j++) {
+            if (inputArray[i] < inputArray[j]) {
+                x = inputArray[i]
+                inputArray[i] = inputArray[j]
+                inputArray[j] = x
+            }
+        }
+    }
+    return inputArray;
+}
+result = sortingOrder(inputArray);
+console.log(result);
+
 document.write(`The numbers are : ${inputArray}`);
-document.write(`<br>The numbers in asceeding order are : ${inputArray.sort()}`);
+document.write(`<br>The numbers in asceeding order are : ${result}`);
 document.write(`<br>The numbers which are removed are : ${inputArray.splice(2, 3)}`);
 document.write(`<br>After removing, the numbers will be : ${inputArray}`);
 
 // console.log(inputArray);
-// console.log(inputArray.sort());
+// console.log(result);
 // console.log(inputArray.splice(2, 3));
 // console.log(inputArray);
